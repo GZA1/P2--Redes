f15fc340852bc621bdf764770c81c60bebf52549
Khalifa96 <37151079+Khalifa96@users.noreply.github.com>
2018-12-24 18:27:32 +0100

    basura

--- P2-Redes/src/p2/redes/P2Redes.java
+++ P2-Redes/src/p2/redes/P2Redes.java
@@ -5,17 +5,63 @@
  */
 package p2.redes;
 
+
+
+import java.io.*;
+import java.net.*;
+
 /**
  *
  * @author gseno
  */
-public class P2Redes {
+public class P2Redes implements Runnable{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private Socket s = null;
+    private InputStream in;
+    private OutputStream out;
+    
+    
+    public P2Redes(Socket s) {
+        this.s = s;
     }
+
+    @Override
+    public void run() {
+      try{
+          
+        out = s.getOutputStream();
+        in = s.getInputStream();
+        byte[] buff = new byte[64];
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int nb = -1;
+        while(nb>0){
+            nb = in.read(buff, 0, 64);
+            baos.write(buff);   
+        }
+        
+        
+        parseMsg(baos);
+      }catch(Exception ex){System.out.println(ex.getMessage());}
+      
+        
+    }
+    
+    private void parseMsg(ByteArrayOutputStream baos){
+        
+        
+    }
+    private void logON(){
+        
+    }
+    private void logOUT(){
+        
+        
+    }
+    private void heartBeat(){
+        
+        
+    }
+    
+    
     
 }
--- P2-Redes/src/p2/redes/Serv.java
+++ P2-Redes/src/p2/redes/Serv.java
@@ -5,10 +5,77 @@
  */
 package p2.redes;
 
+import java.net.*;
+import java.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author gseno
  */
 public class Serv {
+    private int port = 6969;
+    private String ipAdrr= "";
+    private Socket s = null;
+    private ServerSocket ss;
+    private InputStream in;
+    private OutputStream out;
+        private BlockingQueue<Runnable> listaTareas ;
+    private ThreadPoolExecutor executor;
     
+    
+    public Serv(String address, int puerto){
+        try{
+            
+            
+            listaTareas = new LinkedBlockingDeque<>(17);
+            executor = new ThreadPoolExecutor(7, 7, 50, TimeUnit.SECONDS, listaTareas);
+            ss = new ServerSocket(puerto);
+            s=null;
+            while(true){
+                this.s = ss.accept();
+                this.out = new ObjectOutputStream(s.getOutputStream());
+                this.in = new ObjectInputStream(s.getInputStream());
+                
+                executor.execute(new P2Redes(s));
+            }
+        }catch(IOException ex){System.out.println(ex.getMessage());
+        }finally{
+            try{
+                if(this.out!=null) this.out.close();
+                if(this.in!=null) this.in.close();
+                if(this.s!=null) this.s.close();
+                this.out=null;
+                this.in=null;
+                this.s=null;
+            }catch(IOException ex){System.out.println(ex.getMessage());}
+        }
+        
+    }
+    
+    public void main(){
+       
+        
+    }
+    
+   
+        
+        
+        
 }
+    
+    
+    
+    
+    
+    
+    
+
